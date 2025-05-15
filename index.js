f8f0f040d7a36d101ab35df684712a96e3ea68b0
soulefsaoud <87301398+soulefsaoud@users.noreply.github.com>
2025-05-15 13:52:40 +0200

    route d'accueil

---
+++ index.js
@@ -0,0 +1,16 @@
+import 'dotenv/config';
+
+import express from 'express';
+
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req,res) => res.send("Bienvenue"));
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
+});
+
+
